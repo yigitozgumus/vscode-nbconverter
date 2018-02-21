5093668ab19c56405d356f80c0ffb1d06e3a12d9
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2018-02-22 00:03:10 +0100

    it's working

--- extension.js
+++ extension.js
@@ -1,6 +1,6 @@
 
 const vscode = require('vscode');
-
+var PythonShell = require('python-shell');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,13 +14,24 @@ function activate(context) {
     // Now provide the implementation of the command with  registerCommand
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.convertToNotebook', function () {
+
         // The code you place here will be executed every time your command is executed
         let editor = vscode.window.activeTextEditor;
-        let filename = editor.document.fileName;
-        let index = filename.lastIndexOf('/');
-        let directory = filename.substring(0,index+1)
+        let pathName = editor.document.fileName;
+        let index = pathName.lastIndexOf('/');
+        let directory = pathName.substring(0, index + 1)
+        let filename = pathName.substring(index + 1);
+        var options = {
+            mode: 'text',
+            scriptPath: __dirname,
+            args: ['-i', pathName, '-c', '#%%']
+        };
+        PythonShell.run('pytonb.py', options, function (err, results) {
+            if (err) throw err;
+            // results is an array consisting of messages collected during execution
+            console.log('results: %j', results);
+        });     
         console.log(directory)
-        
         // Display a message box to the user
         vscode.window.showInformationMessage('File converted to Jupyter Notebook');
     });
