0e736c9e126d44ba03840967286d8c40654a33e5
Yiğit Özgümüş <yigitozgumus1@gmail.com>
2018-02-26 08:16:19 +0100

    tokenize implementation progress

--- src/convert.js
+++ src/convert.js
@@ -4,10 +4,77 @@ const nbFormat = require("./nbformat.js");
 const fs = require('fs');
 var methods = {};
 //Tokens
+var CODE1 = 'CODE1'; // code line
+var MARK1 = 'MARK1'; // markdown line 
+var MARK2 = 'MARK2'; // markdown block
+var COMM1 = 'COMM1'; // comment line around code
+var COMM2 = 'COMM2'; // multiline comment
+var SEP1 = 'SEP1'; // Empty seperator
+var SEP2 = 'SEP2'; // Filled Seperator
 
 
-methods.tokenize = function tokenize(Data){
+//Methods
+methods.preprocess = function preprocess(inputFile){
+    const global_data = fs.readFileSync(inputFile).toString();
+    const fileData = global_data.split('\n');
+    return fileData;
+};
 
+methods.tokenize = function tokenize(Data,seperator){
+    var tokenizedLines = [];
+    let addFlag = true;
+    let commentMarkCheck = false;
+    let markdownCell = false;
+    for(var index = 0;index < Data.length;index++){
+        let line = Data[index];
+        let elem = {"token": "", "data": ""};
+        if(line.startsWith(seperator)){
+            if(line.length === seperator.length){
+                elem.token = SEP1;
+            }else{
+                elem.token = SEP2;
+            }
+            elem.data = (line + '\n');
+            if (commentMarkCheck) {
+                commentMarkCheck = false;
+            }
+        }
+        else if(line.startsWith("#")){
+            elem.token = COMM1;
+            elem.data = (line + '\n');
+        }else if(line.length === 0){
+            addFlag = false;
+        }else if(line.startsWith("\"\"\"")){
+            elem.token = COMM2;
+            elem.data = (line + '\n');
+            if(commentMarkCheck){
+                commentMarkCheck = false;
+            }else{
+            commentMarkCheck = true;
+            }
+        }else if(commentMarkCheck){
+            if (line.startsWith("#")){
+                tokenizedLines[tokenizedLines.length - 1].token = MARK2;
+                elem.token = MARK2;
+                commentMarkCheck = false;
+                markdownCell = true;
+            }else{
+                elem.token = COMM2;
+            }
+            elem.data = (line + '\n');   
+        }else if(markdownCell){
+            elem.token = MARK2;
+            elem.data = (line + '\n');
+        }else{
+            elem.token = CODE1;
+            elem.data = (line + '\n');
+        }
+        if(addFlag){
+        tokenizedLines.push(elem);
+        }
+    }
+    console.log(tokenizedLines);
+    return tokenizedLines ;
 };
 
 methods.parseCodeCell = function parseCodeCell(){
@@ -22,8 +89,8 @@ methods.translate = function convert_mark2(){
 };
 
 methods.convert = function convert_mark1(file, sep) {
-    const global_data = fs.readFileSync(file).toString();
-    const fileData = global_data.split('\n');
+    
+    var fileData = methods.preprocess(file);
     var notebook = nbFormat.new_notebook();
     const seperator = sep;
     let comment = false;
